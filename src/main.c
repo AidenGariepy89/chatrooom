b8ced9ff89fce4378667cb24f343bcc9fea4fd4e
AidenGariepy89 <AidenGariepy89@gmail.com>
2023-10-18 15:32:44 -0400

    feat: learning c standard library

--- src/main.c
+++ src/main.c
@@ -1,7 +1,31 @@
+#include <stdlib.h> // C Standard Library
 #include <stdio.h>
+#include "socket.h"
+
+void testing();
 
 int main() {
-    printf("Hello\n");
+    testing();
 
     return 0;
+}
+
+int cmpfunc(const void* a, const void* b) { // Compares 2 ints
+    return *(int*)a - *(int*)b;
+}
+
+void testing() {
+    char num_str[] = "54321";
+    int num = atoi(num_str);
+    printf("%d\n", num);
+
+    int vars[] = { 1, 8, 5, 2, 5 };
+    qsort(vars, 5, sizeof(int), cmpfunc); // quick sort
+
+    for (int i = 0; i < 5; i++) {
+        printf("Sorted: %d\n", vars[i]);
+    }
+
+    size_t test = 0;
+    printf("%zu\n", test - 1); // variable overflow
 }
---
+++ src/socket.c
@@ -0,0 +1,5 @@
+#include "socket.h"
+
+int test() {
+    return 5;
+}
